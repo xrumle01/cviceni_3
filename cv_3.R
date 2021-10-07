8e4b77c8dfec856109bfa43f436c4d4d7157df63
Tereza Rumlerova <xrumle01@vutbr.cz>
2021-10-07 13:27:40 +0200

    Add functions VraceniMinci and VraceniMinci_optimal

---
+++ cv_3.R
@@ -0,0 +1,30 @@
+VratitMince <- function(M){
+  padesatek <- floor(M/50)
+  zbytek <- M - 50*padesatek
+  dvacek <- floor(zbytek/20)
+  zbytek <- zbytek - 20*dvacek
+  desitek <- floor(zbytek/10)
+  zbytek <- zbytek - 10*desitek
+  petek <- floor(zbytek/5)
+  zbytek <- zbytek - 5*petek
+  dvojek <- floor(zbytek/2)
+  zbytek <- zbytek - 2*dvojek
+  jednicek <- zbytek
+  vysledek <- c('padesatek:', padesatek, 'dvacek:', dvacek, 'desitek:', desitek, 'petek:', petek, 'dvojek:', dvojek, 'jednicek:', jednicek)
+  return(vysledek)
+}
+
+VratitMince_optimal <- function(M,mince){
+  vratit <- c()
+  while(M > 0){
+    for(i in 1:length(mince)){
+      if (M > mince[i]){
+        x <- floor(M/mince[i])
+        y <- mince[i] * x
+        vratit <- c(vratit, y)
+        M <- M - vratit[length(vratit)]
+      }
+    }
+  }
+  return(vratit)
+}
